4044aebe91a81f43ade570886bb1b823c98aef2c
Aleksandr Volkov <volsashgeo@gmail.com>
2022-10-13 18:17:13 +0300

    feat: add function dateSample(sampleActivity)

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -6,20 +6,28 @@ const HALF_LIFE_PERIOD = 5730;
 /**
  * Determine the age of archeological find by using
  * given MODERN_ACTIVITY and HALF_LIFE_PERIOD values
- * 
- * @param {String} sampleActivity string representation of current activity 
+ *
+ * @param {String} sampleActivity string representation of current activity
  * @return {Number | Boolean} calculated age in years or false
  * in case of incorrect sampleActivity
  *
  * @example
- * 
+ *
  * dateSample('1') => 22387
  * dateSample('WOOT!') => false
  *
  */
-function dateSample(/* sampleActivity */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function dateSample(sampleActivity) {
+  if(typeof(sampleActivity) !== 'string') return false;
+  if(Number(sampleActivity) <= 0) return false;
+  function getBaseLog(y) {
+    return Math.log(y) / Math.log(Math.E);
+  }
+
+  let result = getBaseLog(MODERN_ACTIVITY/Number(sampleActivity)) * HALF_LIFE_PERIOD / getBaseLog(2);
+
+  if(Number(result) > 0) return Math.ceil(result);
+  else return false;
 }
 
 module.exports = {
